b73a90ec6460e46f9113d56c7083da8889fdd03d
Sam Wolski <wolski.samp@gmail.com>
2023-09-10 21:13:50 -0400

    Allowing specifying log levels in launch invocation

--- samsguideclient/cmdline.py
+++ samsguideclient/cmdline.py
@@ -10,6 +10,35 @@ import samsguideclient as sgc
 
 
 ###############################################################################
+## Utility functions
+###############################################################################
+
+## Log level string/value parsing
+
+STR2LOG = {
+	"debug": logging.DEBUG,
+	"info": logging.INFO,
+	"warning": logging.WARNING,
+	"error": logging.ERROR,
+}
+
+def parse_log_level(level_str_or_int):
+	"""Parse a logging level, either as an int or as a string corresponding
+	to a log level from the logging stdlib
+	"""
+	## Check if this is one of the predefined levels
+	if level_str_or_int in STR2LOG:
+		return STR2LOG[level_str_or_int]
+	
+	## Otherwise, try and parse it as an int
+	try:
+		level = int(level_str_or_int)
+	except ValueError as e:
+		raise ValueError(f"Cannot parse {level_str_or_int} as a logging"
+						 " level") from e
+
+
+###############################################################################
 ## Sam's Guide Client
 ###############################################################################
 
@@ -30,6 +59,12 @@ def launch_client():
 	parser.add_argument("--interactive", "-i",
 						help="Enable interactive mode",
 						action="store_true")
+	parser.add_argument("--console-log-level",
+						help="Logging level for console output",
+						default="info")
+	parser.add_argument("--file-log-level",
+						help="Logging level for file output",
+						default="info")
 	cmd_args = parser.parse_args()
 
 
@@ -39,10 +74,11 @@ def launch_client():
 	## Logging
 
 	logger = logging.getLogger("SamsGuideClient")
-	logger = mem_logging.quick_config(logger,
-									  console_log_level=logging.INFO,
-									  file_log_level=logging.DEBUG,
-									  )
+	logger = mem_logging.quick_config(
+				logger,
+				console_log_level=parse_log_level(cmd_args.console_log_level),
+				file_log_level=parse_log_level(cmd_args.file_log_level),
+				)
 	logger.debug("Logging initialized.")
 
 
